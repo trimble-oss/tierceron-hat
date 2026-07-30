50ba45c82f49ff24b870107dd0308b5e806832af
Joel Rieke <joel_rieke@trimble.com>
2026-07-30 09:54:14 -0700

    Increase timeouts

--- cap/cap.go
+++ cap/cap.go
@@ -363,17 +363,17 @@ func getQUICTLSConfigs(encryptPass, encryptSalt string, tlsConfig *FeatherTLSCon
 
 func newFeatherQUICConfig() *quic.Config {
 	return &quic.Config{
-		HandshakeIdleTimeout: 5 * time.Second,
-		MaxIdleTimeout:       30 * time.Second,
+		HandshakeIdleTimeout: 20 * time.Second,
+		MaxIdleTimeout:       2 * time.Minute,
 		KeepAlivePeriod:      10 * time.Second,
 		MaxIncomingStreams:   128,
 	}
 }
 
 func dialQUICConn(addr string, clientTLSConfig *tls.Config) (*quic.Conn, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	quicConn, err := quic.DialAddr(ctx, addr, clientTLSConfig, newFeatherQUICConfig())
+	defer cancel()
 	if err != nil {
 		return nil, err
 	}
@@ -381,9 +381,9 @@ func dialQUICConn(addr string, clientTLSConfig *tls.Config) (*quic.Conn, error) 
 }
 
 func openQUICStream(quicConn *quic.Conn) (net.Conn, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	stream, err := quicConn.OpenStreamSync(ctx)
+	defer cancel()
 	if err != nil {
 		return nil, err
 	}
