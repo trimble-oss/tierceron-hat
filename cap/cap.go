38047be4ab52cc2411c751bd15523ec1ad13dfd6
Joel Rieke <joel_rieke@trimble.com>
2026-07-31 14:38:22 -0700

    Revert unnecessary change

--- cap/cap.go
+++ cap/cap.go
@@ -299,9 +299,6 @@ func getQUICTLSConfigs(encryptPass, encryptSalt string, tlsConfig *FeatherTLSCon
 
 	allowSelfSigned := tlsConfig == nil || tlsConfig.AllowSelfSigned
 	serverName := featherQUICServerName
-	if tlsConfig != nil && len(*tlsConfig.ServerName) > 0 {
-		serverName = *tlsConfig.ServerName
-	}
 	if tlsConfig != nil && tlsConfig.ServerName != nil && len(*tlsConfig.ServerName) > 0 {
 		serverName = *tlsConfig.ServerName
 	}
